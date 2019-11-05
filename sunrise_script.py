a8ab7856e99c02f6945207c2adc7b3721942ea8e
Roman Kiselev <roman.kiselew@gmail.com>
2019-11-05 14:39:18 -0600

    Added pulsing alarm

--- sunrise_script.py
+++ sunrise_script.py
@@ -34,9 +34,15 @@ phase2_transitions = [
 
     # Bright cool white
     TemperatureTransition(6000, duration=6*MIN, brightness=100),
-    TemperatureTransition(1800, duration=100, brightness=1),
 ]
 
+alarm_transitions = [
+    TemperatureTransition(6000, duration=60, brightness=1),
+    TemperatureTransition(6000, duration=140, brightness=100),
+    TemperatureTransition(6000, duration=60, brightness=1),
+    TemperatureTransition(6000, duration=140, brightness=100),
+    SleepTransition(600)
+]
 
 # PARSE COMMAND LINE ARGUMENTS AND SET LOGGING LEVEL
 parser = argparse.ArgumentParser(
@@ -46,6 +52,10 @@ parser.add_argument("-v", "--verbose", action="count",
                     help="increase output verbosity")
 parser.add_argument("-d", "--duration", action="store_true",
                     help="print duration of the whole sunrise and exit")
+parser.add_argument("-a", "--alarm", type=int, default=0,
+                    help="# of light pulses for alarm after the sunrise")
+parser.add_argument("-n", "--no_sunrise", action="store_true",
+                    help="disable sunrise, use only alarm, if set")
 args = parser.parse_args()
 level = logging.WARNING
 if args.verbose == 1:
@@ -63,6 +73,14 @@ def activate_bulb(bulb, duration=POWER_ON_DURATION):
     bulb.set_hsv(1, 100, 1, effect='smooth', duration=duration)
     sleep(duration)
 
+# Get bulb by name
+def get_bulb(name, bulbs):
+    match = [b for b in bulbs if b['capabilities']['name'] == name]
+    if len(match) == 1:
+        return Bulb(match[0]['ip'], auto_on=True)
+    else:
+        return None
+
 def lamp_thread(lamp, delay, bulbs):
     """
     This function turns on the lamp, calculates
@@ -73,9 +91,8 @@ def lamp_thread(lamp, delay, bulbs):
     info  = lambda m: logging.info   ('I: %10s: %s' % (lamp, m))
     debug = lambda m: logging.debug  ('D: %10s: %s' % (lamp, m))
     
-    match = [b for b in bulbs if b['capabilities']['name'] == lamp]
-    if len(match) == 1:
-        bulb = Bulb(match[0]['ip'], auto_on=True)
+    bulb = get_bulb(lamp, bulbs)
+    if bulb:
         bulb.turn_off()
     else:
         warn("Lamp not found on the network")
@@ -107,6 +124,17 @@ def lamp_thread(lamp, delay, bulbs):
         )
     )
 
+    if args.alarm:
+        sleep(sum([a.duration for a in transitions]))
+
+
+def alarm(lamp, bulbs):
+    bulb = get_bulb(lamp, bulbs)
+    bulb.start_flow(Flow(
+        count=args.alarm,
+        action=Flow.actions.recover,
+        transitions=alarm_transitions))
+
 
 def main():
     # Sanity check
@@ -124,14 +152,16 @@ def main():
     bulbs = discover_bulbs()
     logging.info('%i lamp(s) found' % len(bulbs))
 
-    #lamp_thread(LAMP_DELAYS.popitem(), bulbs)
-    #return
-    
-    with ThreadPoolExecutor() as executor:
-        f = lambda it: lamp_thread(*it, bulbs)
-        executor.map(f, LAMP_DELAYS.items())
+    if not args.no_sunrise:
+        with ThreadPoolExecutor() as executor:
+            f = lambda it: lamp_thread(*it, bulbs)
+            executor.map(f, LAMP_DELAYS.items())
 
-    alarm()
+    if args.alarm:
+        logging.info("Triggering alarm")
+        with ThreadPoolExecutor() as executor:
+            f = lambda it: alarm(it, bulbs)
+            executor.map(f, LAMP_DELAYS)
 
 
 if __name__ == '__main__':
